04d3b0ecf56dc9b7a6a73ee7fd1058b5a9c21e6d
Chris Luiz <chris@chrisluiz.io>
2017-09-01 21:12:54 -0400

    torch nn

--- torch_tst.py
+++ torch_tst.py
@@ -1,6 +1,11 @@
 import torch
 import torchvision
 import torchvision.transforms as transforms
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+import numpy as np
 
 transform = transforms.Compose(
     [transforms.ToTensor(),
@@ -25,3 +30,38 @@ testloader = torch.utils.data.DataLoader(testset,
 
 classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+
+def imshow(img):
+    img = img / 2 + 0.5
+    npimg = img.numpy()
+    plt.imshow(np.T(npimg, (1, 2, 0)))
+
+
+data_iter = iter(trainloader)
+images, labels = data_iter.next()
+
+imshow(torchvision.utils.make_grid(images))
+print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__inti__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        return x
+
+
+net = Net()
