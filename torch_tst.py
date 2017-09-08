d5cd810a98a086007c2714a161a915bf00798e71
Chris Luiz <chris@chrisluiz.io>
2017-09-07 20:48:35 -0400

    minor fixes

--- torch_tst.py
+++ torch_tst.py
@@ -36,7 +36,7 @@ classes = ('plane', 'car', 'bird', 'cat',
 def imshow(img):
     img = img / 2 + 0.5
     npimg = img.numpy()
-    plt.imshow(np.T(npimg, (1, 2, 0)))
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
 
 
 data_iter = iter(trainloader)
@@ -48,7 +48,7 @@ print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
 
 class Net(nn.Module):
     def __init__(self):
-        super(Net, self).__inti__()
+        super(Net, self).__init__()
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
@@ -65,69 +65,81 @@ class Net(nn.Module):
         return x
 
 
-net = Net()
+def main():
+    net = Net()
 
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    data_iter = iter(trainloader)
+    images, labels = data_iter.next()
 
-for epoch in range(2):
-    running_loss = 0.0
-    for i, data, in enumerate(trainloader, 0):
-        # get inputs
-        inputs, labels = data
-        # wrap inputs in variable
-        inputs, labels = Variable(inputs), Variable(labels)
-        # zero parameter gradients
-        optimizer.zero_grad()
+    imshow(torchvision.utils.make_grid(images))
+    print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
 
-        # forward + backward optimize
-        outputs = net(inputs)
-        loss = criterion(outputs, labels)
-        loss.backword()
-        optimizer.step()
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-        # print stats
-        running_loss += loss.data[0]
-        if i % 2000 == 1999:
-            print('[%d, %5d] loss: %.3f' % (epoch + 1,
-                                            i + 1,
-                                            running_loss / 2000))
-            running_loss = 0.0
+    for epoch in range(2):
+        running_loss = 0.0
+        for i, data, in enumerate(trainloader, 0):
+            # get inputs
+            inputs, labels = data
+            # wrap inputs in variable
+            inputs, labels = Variable(inputs), Variable(labels)
+            # zero parameter gradients
+            optimizer.zero_grad()
 
-print('Finished Training!')
+            # forward + backward optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
 
-outputs = net(Variable(images))
+            # print stats
+            running_loss += loss.data[0]
+            if i % 2000 == 1999:
+                print('[%d, %5d] loss: %.3f' % (epoch + 1,
+                                                i + 1,
+                                                running_loss / 2000))
+                running_loss = 0.0
 
-_, predicted = torch.max(outputs.data, 1)
-print('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
-                              for j in range(4)))
+    print('Finished Training!')
 
-correct = 0
-total = 0
-for data in testloader:
-    images, labels = data
     outputs = net(Variable(images))
+
     _, predicted = torch.max(outputs.data, 1)
-    total += labels.size(0)
-    correct += (predicted == labels).sum()
+    print('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
+                                  for j in range(4)))
 
-print('Accuracy of the network on the 10000 test images: %d %%' % (
-       100 * correct / total))
+    correct = 0
+    total = 0
+    for data in testloader:
+        images, labels = data
+        outputs = net(Variable(images))
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum()
 
-class_correct = list(0. for i in range(10))
-class_total = list(0. for i in range(10))
-for data in testloader:
-    images, labels = data
-    outputs = net(Variable(images))
-    _, predicted = torch. max(outputs.data, 1)
-    c = (predicted == labels).squeeze()
-    for i in range(4):
-        label = labels[i]
-        class_correct[label] += c[i]
-        class_total[label] += 1
+    print('Accuracy of the network on the 10000 test images: %d %%' % (
+           100 * correct / total))
 
+    class_correct = list(0. for i in range(10))
+    class_total = list(0. for i in range(10))
+    for data in testloader:
+        images, labels = data
+        outputs = net(Variable(images))
+        _, predicted = torch.max(outputs.data, 1)
+        c = (predicted == labels).squeeze()
+        for i in range(4):
+            label = labels[i]
+            class_correct[label] += c[i]
+            class_total[label] += 1
 
-for i in range(10):
-    print('Accuracy of %5s : %2d %%' % (
-        classes[i], 100 * class_correct[i] / class_total[i]))
-    
+    for i in range(10):
+        print('Accuracy of %5s : %2d %%' % (
+            classes[i], 100 * class_correct[i] / class_total[i]))
+
+    #net.cuda()
+
+    #inputs, labels = Variable(inputs.cuda()), Variable(labels.cuda())
+
+if __name__ == '__main__':
+    main()
